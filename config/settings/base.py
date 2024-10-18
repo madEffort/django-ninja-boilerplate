b539ad5bebfc2b61323a41d78bdd92b6aeb36f8d
Tobias Kim <madeffort0903@gmail.com>
2024-10-18 14:05:49 +0900

    clean boilerplate

--- config/settings/base.py
+++ config/settings/base.py
@@ -128,8 +128,8 @@ AUTH_USER_MODEL = "users.User"
 
 # https://eadwincode.github.io/django-ninja-jwt/
 NINJA_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
